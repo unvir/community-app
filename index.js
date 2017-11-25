3102c34dd12de7bac50133f95cd3a4e5d959b01c
Gilevich Petr <unvir@yandex.ru>
2017-11-26 01:39:40 +0300

    Events listener in progress

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Notify = {
     container: document.getElementById('Notify'),
-    generate: function(title){
+    generate: function(title) {
         var msg_container = document.createElement('div');
             msg_container.className = 'container_notify';
             msg_container.innerHTML = '<span class="title">' + title + '</span>';
@@ -35,14 +35,99 @@ var Notify = {
     }
 };
 
+var VkEvents = {
+    btnSubscribe: document.getElementById('subscribe-event'),
+    listen: [],
+    init: () => {
+        var eventSelector = document.getElementById('picker-event');
+
+        // Изменение события в выпадающем списке
+        eventSelector.addEventListener('change', () => {
+            var pickedEvent = document.getElementById('picker-event').value;
+
+            if(!VkEvents.isAvailable(pickedEvent))
+                return false;
+
+            // если событие прослушивается
+            if(~VkEvents.listen.indexOf(pickedEvent)){
+                VkEvents.btnSubscribe.innerHTML = 'Отписаться';
+            } else {
+                VkEvents.btnSubscribe.innerHTML = 'Подписаться';
+            }
+
+            document.getElementById('jscode-event').innerHTML = VkEvents.AvailableEvents[pickedEvent];
+        });
+
+        // Обработка нажатия на кнопку
+        VkEvents.btnSubscribe.addEventListener('click', () => {
+            var pickedEvent = document.getElementById('picker-event').value;
+
+            try {
+
+                // если событие в списке прослушиваемых
+                if(~VkEvents.listen.indexOf(pickedEvent)){
+                    VkEvents.unsubscribe(pickedEvent);
+                    VkEvents.btnSubscribe.innerHTML = 'Подписаться';
+                } else {
+                    VkEvents.subscribe(pickedEvent);
+                    VkEvents.btnSubscribe.innerHTML = 'Отписаться';
+                }
+
+            } catch (e) {
+                Notify.generate('Event listener', e.message);
+            }
+
+        });
+    },
+    subscribe: (eventname) => {
+        if(VkEvents.isAvailable(eventname)){
+            eval(VkEvents.AvailableEvents[eventname]);
+            VkEvents.listen.push(eventname);
+        }
+    },
+    unsubscribe: (eventname) => {
+        VkEvents.listen.splice( VkEvents.listen.indexOf(pickedEvent), 1 );
+
+        //TODO Храинлище функций обработчиков
+    },
+    isAvailable: (eventname) => {
+        if(eventname in VkEvents.AvailableEvents)
+            return true;
+        return false;
+    },
+    AvailableEvents: {
+        onLocationChanged: "VK.addCallback('onLocationChanged', function f(location){\n\talert(\"location: \" + location);\n});",
+        test: "console.log('test complete')"
+    }
+}
+
 window.addEventListener("load", () => {
+    function locationHashChanged() {
+        var titleBox = document.getElementById('title');
+        switch(location.hash) {
+            case '#page-main':
+                titleBox.innerHTML = '<h1>Что это?</h1>';
+            break;
+            case '#page-api':
+                titleBox.innerHTML = '<h1>Методы</h1>';
+            break;
+            case '#page-event':
+                titleBox.innerHTML = '<h1>События</h1>';
+            break;
+        }
+    }
+
+    location.hash = '#page-main';
+    window.onhashchange = locationHashChanged;
+    VkEvents.init();
+
     try{
         VK.init(function() {
             Notify.generate('JS SDK Connection', 'success');
         }, function() {
             Notify.generate('JS SDK Connection', 'fail');
-        });
+        }, '5.69');
     } catch(e) {
         Notify.generate('Error', e.message);
     }
-}, '5.69');
+});
